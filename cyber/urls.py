74c55c261f0fe2594de3b1a374d0a88c1e5dbdd1
Sreelakshmi-c <csreelakshmi2@gmail.com>
2022-01-17 13:41:10 +0530

    herokuuu

--- cyber/urls.py
+++ cyber/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path('cyberr/',views.fnCyberr,name='cyberr/')
+    path('',views.fnCyberr,name='cyberr/')
 ]
--- cybersquare/urls.py
+++ cybersquare/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('demo/',include('cyber.urls'))
+    path('',include('cyber.urls'))
 ]
